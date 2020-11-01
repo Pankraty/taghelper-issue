57e7ec4657d3173bdb2788141ed55d2900e0efe4
Aleksei <pankraty@gmail.com>
2020-11-01 16:25:36 +0400

    Add sample TagHelper (FAILURE)

---
+++ SampleApp/TagHelpers/SampleTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SampleApp.TagHelpers
+{
+    [HtmlTargetElement("input", TagStructure = TagStructure.WithoutEndTag)]
+    public class SampleTagHelper : InputTagHelper
+    {
+        public SampleTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+        }
+    }
+}
